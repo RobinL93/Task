1299cbca0b8008597ce749a892b3c8a92addbda1
RobinL93 <robin.larsson@infocaption.com>
2015-05-10 16:24:07 +0200

    no message

--- home/js/script.js
+++ home/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 	// get the taaaaskz
+	
 	$.ajax({
 
 		url: "http://localhost/task_manager/v1/tasks",
@@ -13,9 +14,14 @@ $(document).ready(function(){
 			//console.log(data);
 			taskHtml = "";
 			$.each(data.tasks, function(i, item) {
-				taskHtml += "<div class='box' id='"+item.id+"'>";
+				taskHtml += "<div class='taskBox' id='"+item.id+"'>";
 				taskHtml += "<p class='taskText'>" + item.task + "</p>";
-				taskHtml += "<p class='createdAt'>" + item.createdAt + "</p>";
+				taskHtml += "<div class='taskFooter'>";
+				taskHtml += "<p class='createdAt'>" + item.createdAt + "</p> ";
+				taskHtml += "<p class='taskOptions'> ";
+				taskHtml += "<i class='fa fa-pencil-square-o edit'></i>";
+				taskHtml += "<i class='fa fa-times delete'></i>";
+				taskHtml += "</p></div>";
 				taskHtml += "</div>";
 			});
 
@@ -27,8 +33,58 @@ $(document).ready(function(){
 
 	});
 
-	$("#wrapper").on("click", '.box', function(){
-		console.log("box clicked");
+
+	// Listeners for delete and edit
+
+	$("#wrapper").on("click", '.delete', function(e){
+
+		if (confirm("Do you want to delete this task? ")) {
+			var taskBox = $(this).parents(".taskBox");
+			var taskBoxId = taskBox.attr("id");
+
+			// Delete
+			$.ajax({
+				url: "http://localhost/task_manager/v1/tasks/" + taskBoxId,
+				method: "DELETE",
+				dataType: "json",
+				cache: false,
+				headers: {
+				'Authorization' : '1f324bafd2df1db785bd413ea5c120c3'
+				},
+				success: function(){
+					$("#wrapper").load(location.href + "#wrapper");
+					infoBar("Successfully deleted", "seagreen");
+				},
+				error: function(){
+					infoBar("Could not delete task.", "red");
+				}
+
+			});
+		} else {
+			return;
+		}
+	});
+
+	$("#wrapper").on("click", '.edit', function(){
+		
+		
+
 	});
 
 });
+
+function infoBar(message, textColor){
+
+	tTime = 2500;
+	$(".infoBar").slideDown();
+	$(".infoBar").html("<p>" + message + "</p>");
+	
+	$(".infoBar").css({
+		color: textColor
+	});
+
+	setTimeout(function(){
+		$(".infoBar").slideUp().html("");
+	}, tTime);
+
+}
